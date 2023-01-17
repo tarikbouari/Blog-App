39c9bca6b637ca8500d740fbc4e1d501888ef8ea
tarikbouari <tarikbouari@gmail.com>
2023-01-17 19:59:47 +0000

    Generate schema file

--- db/migrate/20230117163018_add_user_ref_to_posts.rb
+++ db/migrate/20230117163018_add_user_ref_to_posts.rb
@@ -1,5 +1,5 @@
 class AddUserRefToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_references :posts, :author, foreign_key: true
+    add_reference :posts, :user, foreign_key: true
   end
 end
