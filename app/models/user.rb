1161ac912fe3ef5e7e307b91fe95ae8cfe8c6bbd
tarikbouari <tarikbouari@gmail.com>
2023-01-21 16:48:56 +0000

    Add validations for users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true,  comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
   has_many :posts
   has_many :comments
   has_many :likes
