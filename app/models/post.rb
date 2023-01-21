5bc93bbadd90cc56b669d2eeb648a55559a22013
tarikbouari <tarikbouari@gmail.com>
2023-01-21 20:34:54 +0000

    Fix linters error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-    validates :title, presence: true, length: { in: 1..250 }
-    validates :comments_counter, :likes_counter, comparison: { greater_than_or_equal_to: 0 },
-                                                 numericality: { only_integer: true }
+  validates :title, presence: true, length: { in: 1..250 }
+  validates :comments_counter, :likes_counter, comparison: { greater_than_or_equal_to: 0 },
+                                               numericality: { only_integer: true }
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
