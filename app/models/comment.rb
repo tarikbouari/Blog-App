147f79191478f7a810329dd131cc31bf79de34b7
tarikbouari <tarikbouari@gmail.com>
2023-01-21 16:08:28 +0000

    Refactor code and fix linters error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
+  belongs_to :user
+  belongs_to :post
 
-    after_save :comments_count
-    
-    def comments_count
-        post.increment!(:comments_counter)
-    end
+  after_save :comments_count
+
+  def comments_count
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,15 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    has_many :comments
-    has_many :likes
-    
-    after_save :posts_count
+  belongs_to :user
+  has_many :comments
+  has_many :likes
 
-    def posts_count
-       user.increment!(:posts_counter)
-    end
-    def recent_comments
-        comments.order(created_at: :desc).limit(5)
-    end
+  after_save :posts_count
+
+  def posts_count
+    user.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
