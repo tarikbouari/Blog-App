a0220bfe5e9f04ac88f9b8d3e6154bb6a9a4c5f4
tarikbouari <tarikbouari@gmail.com>
2023-01-21 20:33:47 +0000

    Install rails-spec gem for testing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, length: { in: 1..250 }
+    validates :comments_counter, :likes_counter, comparison: { greater_than_or_equal_to: 0 },
+                                                 numericality: { only_integer: true }
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
--- db/migrate/20230121182657_change_foreign_key_for_posts.rb
+++ db/migrate/20230121182657_change_foreign_key_for_posts.rb
@@ -1,5 +1,5 @@
 class ChangeForeignKeyForPosts < ActiveRecord::Migration[7.0]
   def change
-    rename_column :posts, :author_id, :author_id
+    rename_column :posts, :user_id, :author_id
   end
 end
