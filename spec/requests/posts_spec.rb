0517999dd6a4cd4c0ad990fe88e952dd5586d5b6
tarikbouari <tarikbouari@gmail.com>
2023-01-25 17:54:42 +0000

    Add test for post controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET #index' do
+    it 'sould test the response' do
+      # another way for getting the route is get users_path
+      get '/users/user_id/posts'
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:index)
+      expect(response.body).to include(' Here is a list of Post')
+    end
+  end
+
+  describe 'GET #show' do
+    it 'sould test the response for show' do
+      get '/users/user_id/posts/:id'
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+      expect(response.body).to include('Post profile')
+    end
   end
 end
