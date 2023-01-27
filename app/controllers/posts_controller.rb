f4f9fa526122bc24b61e31ccdf822fe600890492
tarikbouari <tarikbouari@gmail.com>
2023-01-27 20:07:56 +0000

    Add creat action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,19 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
     
+    respond_to do |format|
+      format.html do
+        if @post.save
+          redirect_to user_post_path(current_user, @post)
+        else
+          redirect_to new_user_post_path(current_user)
+        end
+      end
+    end
+  end
+  def post_params
+    params.require(:post).permit(:title, :body).merge(author: current_user, comments_counter: 0, likes_counter: 0)
   end
 end
