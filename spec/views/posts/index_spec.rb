4917bc3168fbc0d1f43f2b892a0e4019c2af7378
tarikbouari <tarikbouari@gmail.com>
2023-01-31 16:36:11 +0000

    Add test for user post index page

--- spec/views/posts/index_spec.rb
+++ spec/views/posts/index_spec.rb
@@ -2,19 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Post Index', type: :feature do
   before :each do
-    @avatar_pic = 'https://unsplash.com/photos/F_-0BxGuVvo'
+    @pic = 'https://unsplash.com/photos/F_-0BxGuVvo'
     @author = User.create(name: 'Cassian Andor',
-                          photo: @avatar_pic,
+                          photo: @pic,
                           bio: 'Rebel fighter from Kenari.An accomplished Alliance Intelligence officer
                           with combat field experience.')
     @first_post = Post.create(author: @author, title: 'Selam from', text: 'This is my first post')
     @second_post = Post.create(author: @author, title: 'Hello there!',
-                               text: 'This is ObiWans\' message: Don\'t let the Empire win')
+                               text: 'This is james')
 
-    @com1 = Comment.create(author: @author, post: @second_post, text: 'Thank you for your support 😊')
-    @com2 = Comment.create(author: @author, post: @second_post,
-                           text: 'Hello there Cassian, this Dero I am watching you')
-    @com3 = Comment.create(author: @author, post: @second_post, text: 'Spend the credits wisely :|')
+    @comment = Comment.create(author: @author, post: @second_post, text: 'Thank you')
+   
 
     visit user_posts_path(@author)
   end
@@ -23,7 +21,7 @@ RSpec.describe 'Post Index', type: :feature do
     expect(page).to have_content(@author.name)
   end
   it 'shows the user photo' do
-    expect(page.body).to include(@avatar_pic)
+    expect(page.body).to include(@pic)
   end
 
   it 'shows number of posts user has written' do
@@ -31,7 +29,7 @@ RSpec.describe 'Post Index', type: :feature do
   end
 
   it 'shows number of comments a post has' do
-    expect(page.body).to include('Comments: 3')
+    expect(page.body).to include('Comments: 1')
   end
 
   it 'shows number of likes a post has' do
@@ -42,7 +40,7 @@ RSpec.describe 'Post Index', type: :feature do
     expect(page).to have_content(@second_post.title)
   end
 
-  it 'shows part of a post body (This is ObiWans\' message: Don\'t let the Empire win)' do
+  it 'shows part of a post body ' do
     expect(page).to have_content(@second_post.text)
   end
 
@@ -50,4 +48,13 @@ RSpec.describe 'Post Index', type: :feature do
     click_on @first_post.title
     expect(page).to have_current_path(user_post_path(@author, @first_post))
   end
+
+  it 'to show the Pagination buttons' do
+    expect(page).to have_content('Pagination')
+  end
+
+  it 'shows the users first comment on a post ' do
+    expect(page).to have_content(@comment.text) 
+  end
+
 end
