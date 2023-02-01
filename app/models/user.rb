8c653257caae59bdd72471ef9c23e6ce580d476e
tarikbouari <tarikbouari@gmail.com>
2023-02-01 15:54:12 +0000

    Add validation for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # validates :name, presence: true
-  # validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+  # Include default devise modules. Others available are:
+  #  :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+ 
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
