4deffbc0afbb15b4c116e3ea055a260ec524a58d
tarikbouari <tarikbouari@gmail.com>
2023-04-01 18:49:35 +0000

    Update model files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :posts
+  belongs_to :post
 
   after_save :comments_count
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
-  has_many :like
+  has_many :likes
 
   after_save :posts_count
 
