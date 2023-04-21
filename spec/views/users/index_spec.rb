5fe383bc65313cbcd2ecdfb7b91e9745c412c1d2
tarikbouari <tarikbouari@gmail.com>
2023-04-21 11:42:58 +0000

    Upadate test

--- spec/views/users/index_spec.rb
+++ spec/views/users/index_spec.rb
@@ -1,33 +1,59 @@
+# require 'rails_helper'
+
+# RSpec.describe 'Post Show', type: :feature do
+#   before :each do
+#     @avatar_pic = 'https://unsplash.com/photos/F_-0BxGuVvo'
+
 require 'rails_helper'
 
-RSpec.describe 'Post Show', type: :feature do
-  before :each do
-    @avatar_pic = 'https://unsplash.com/photos/F_-0BxGuVvo'
-    @author = User.create(name: 'Cassian Andor',
-                          photo: @avatar_pic,
-                          bio: 'Rebel fighter from Kenari.An accomplished Alliance Intelligente',
-                          posts_counter: 0)
+RSpec.describe 'users/index.html.erb', type: :feature do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @first_user = User.create(name: 'david ferra', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                              bio: 'Ruby developer from congo')
+    @second_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                               bio: 'Teacher from Mexico.')
+    @third_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                              bio: 'Teacher from Poland.')
+    @first_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my first post')
+    @second_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my first post')
+    Comment.create(post: @first_post, author: @second_user, text: 'Hi Tom!')
+    Comment.create(post: @first_post, author: @second_user, text: 'Hi Tom!')
+    Like.create(post: @first_post, author: @first_user)
 
-    @post = Post.create(author: @author, title: 'Hello there!',
-                        text: 'This is ObiWans\' message: Don\'t let the Empire win')
-
-    visit root_path
+    @users = User.all
+  end
+  it 'user index shows all the user names' do
+    visit users_path
+    @users.each do |user|
+      expect(page).to have_content(user.name)
+    end
   end
 
-  it 'shows the username of the user' do
-    expect(page).to have_content(@author.name)
+  it 'Shows the image of all users' do
+    visit users_path
+    @users.each do |user|
+      expect(page).to have_css("img[src*='#{user.photo}']")
+    end
   end
 
-  it 'contains users pic' do
-    expect(page).to have_css("img[src='#{@avatar_pic}']")
+  it 'Shows the text \'Number of posts\' for all users' do
+    visit users_path
+    @users.each do |_user|
+      expect(page).to have_content('Number of posts:')
+    end
   end
 
-  it 'Count user post' do
-    expect(page.body).to include('Number of posts: 1')
+  it 'Shows the posts_counter of all users' do
+    visit users_path
+    @users.each do |user|
+      expect(page).to have_content(user.posts_counter)
+    end
   end
 
-  it 'Directed to show user page' do
-    click_link @author.name
-    expect(page).to have_current_path(user_path(@author))
+  it 'Redirects to user\'s show page' do
+    visit users_path
+    click_link @first_user.name
+    expect(current_path).to match user_path(@first_user)
   end
 end
